e0f24d6fd736d1beef61739e85a328b45dffd573
DylanKrajnc <dylan_krajnc@outlook.com>
2026-04-05 03:10:45 +0100

    Section 8 - Lecture 81

---
+++ Section_8/memcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char *argv[])
+{
+  char text1[] = "Hello there";
+
+  const int BUFFSIZE = strlen(text1) + 1;
+  char text2[BUFFSIZE];
+
+  memcpy(text2, text1, BUFFSIZE);
+
+  puts(text2);
+
+  return 0;
+}
